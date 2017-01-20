9a1306b2a8d663b20466effb7f917c0c35b09846
mao <o5man0ff@yandex.ru>
2017-01-20 16:59:48 +0300

    Поле masterday класса SectionFields было неверно записано как masterDay, вызывая остановку дальнейшей обработки списка backup.conf

--- src/tm/mao/cbf/CBFIni.java
+++ src/tm/mao/cbf/CBFIni.java
@@ -34,39 +34,25 @@ public class CBFIni {
         private static Logger log = Logger.getLogger(CBFIni.class.getName());
 
         public CBFIni() {
-
 		try {
-		
 			List<String> lines = Files.readAllLines(Paths.get("backups.conf"));
 			boolean checkSection = false;
-			int i = -1;
 
 			for(String line: lines){
 				if (line.length() > 0 && line.charAt(0) != '#') {
-					if (!checkSection) {
-						checkSection = true; // Отметка, что начались параметры секции
-						obj = new SectionFields();
-						sectionData.add(obj); // Создаем новый элемент-объект списка
+					if (!checkSection) { // Если секция еще не начиналась
+						checkSection = true; // Отмечаем, что встретили секцию
+						obj = new SectionFields(); // создаем объект класса с используемыми нами параметрами
+						sectionData.add(obj); // Создаем в списке новый элемент с этим объектом
 					}
-					i++;
-					matchParam = patternParam.matcher(line); // Выуживаем имя параметра
-//					matchValue = patternValue.matcher(line); // Выуживаем значение параметра
-					log.debug("line = " + line);
-					if (matchParam.lookingAt()) {
-						setters = getSetters(SectionFields.class); //
-//						obj = new SectionFields();
+					matchParam = patternParam.matcher(line); // Выуживаем из строки имя параметра
+					if (matchParam.lookingAt()) { // если совпадение найдено (т.е. это строка с параметром)
+						setters = getSetters(SectionFields.class); // вызываем метод создания метода заполнения значением поля класса с тем же именем, что и параметр
 						setters.get(line.substring(matchParam.start(),matchParam.end()).toLowerCase()).invoke(obj, line.substring(matchParam.end()+1)); // находит поле и заполняет его значением (выполняет метод заполнения данного поля значением) 
 					}
 				} else {
 					checkSection = false;
-					i = -1;
 				}
-			}
-
-//			тестовый блок вывода всех значений списка
-			for(SectionFields sectionField: sectionData){
-				log.info(sectionField.backup);
-				log.info(sectionField.description);
 			}
 		} catch (Exception e) {
 		} catch (Throwable e) {
@@ -89,7 +75,7 @@ public class CBFIni {
 	}
 
 	public class SectionFields {
-		String backup, description, type, server, folder, days, weeks, monthes, years, masterDay;
+		String backup, description, type, server, folder, days, weeks, monthes, years, masterday;
 	}
 
 }
