109dd4e60d50cbe844b1a650a8cd1118f52020d7
mao <o5man0ff@yandex.ru>
2017-01-23 23:05:28 +0300

    Упрощение настроечного файла бэкапов и работа над секциями ежедневных и недельных бэкапов (не завершено, есть ошибки повторного заведения в список уже просмотренных файлов)

--- src/tm/mao/cbf/ProcessingBackups.java
+++ src/tm/mao/cbf/ProcessingBackups.java
@@ -18,12 +18,13 @@ public class ProcessingBackups {
 			Calendar currentDate = new GregorianCalendar(); // текущая дата
 			Long currentDateInMs = currentDate.getTimeInMillis();
 			Long diffTime;
-			int currentDOW; // текущий день недели
+			int currentDOW, masterDay; // текущий день недели
 			SmbFile smbFile;
 
 			for(SectionFields sectionFields: iniBckObj.sectionData) { //перебор списка с данными для бэкапов
 				smbFile = new SmbFile("smb://" + sectionFields.server + "/", sectionFields.folder + "/", auth); // список файлов
 
+				// Вычисление ежедневных копий
 				if (sectionFields.days.replaceAll(" ", "") != "") { // если задано число дней
 					diffTime = Long.parseLong(sectionFields.days) * 3600 * 24 * 1000;
 					for ( SmbFile f : smbFile.listFiles() ) { // перебираем список файлов
@@ -39,21 +40,34 @@ public class ProcessingBackups {
 				// теперь currentDate указывает на дату самого раннего дневного бэкапа + 1 день (если дневные заданы)
 				// начинаем искать вниз по дате ближайший опорный день, с которого начнем отсчитывать недельные бэкапы
 
-
+				// Вычисление еженедельных копий
 				if (sectionFields.weeks.replaceAll(" ", "") != "") { // если задано число недель
-					diffTime = Long.parseLong(sectionFields.days) * 3600 * 24 * 1000;
+					diffTime = Long.parseLong(sectionFields.weeks) * 3600 * 24 * 1000;
 					currentDOW = currentDate.get(Calendar.DAY_OF_WEEK); // какой на текущей дате день недели
-					if (currentDOW != sectionFields.masterday) { // если текущий день недели не опорный
+					masterDay = Integer.parseInt(sectionFields.masterday);
+					currentDate.add(Calendar.DAY_OF_MONTH, masterDay - currentDOW);
+					if (currentDOW - masterDay >= 0) { // если день недели больше опорного или равен ему
+						currentDate.add(Calendar.DAY_OF_MONTH, masterDay - currentDOW);
+					} else {
+						currentDate.add(Calendar.DAY_OF_MONTH, masterDay - currentDOW - 7);
+					} // Теперь currentDate указывает на дату самого позднего недельного бэкапа
+					currentDateInMs = currentDate.getTimeInMillis();
 
-					}
-					for ( SmbFile f : smbFile.listFiles() ) { // перебираем список файлов
-						if (f.createTime() >= (currentDateInMs - diffTime) ) { // если файл попадает в интервал дат количества файлов
-							essentialFiles.add(f.getName());
+					for (int i = 0; i < Integer.parseInt(sectionFields.weeks); i++) { // Перебор всех недельных бэкапов
+						currentDate.add(Calendar.DAY_OF_MONTH, - i * 7); // Идем каждый раз на неделю назад, начиная с текущей
+						currentDateInMs = currentDate.getTimeInMillis();
+						for ( SmbFile f : smbFile.listFiles() ) { // перебираем список файлов
+							if ((f.createTime() / (1000 * 3600 * 24)) == (currentDateInMs / (1000 * 3600 * 24))) {
+								essentialFiles.add(f.getName());
+							}
 						}
 					}
 				}
-		
 
+	                        for(String s: essentialFiles) {
+					log.info(s);
+				}
+		
 
 /*				log.info(String.format("%40s", "").replace(' ', '-'));
 				log.info((char)27 + "[93m" + sectionFields.backup + " -- " + sectionFields.description + " (срок давности - " + sectionFields.days + " сут., путь - smb://" + sectionFields.server + "/" + sectionFields.folder + (char)27 + "[0m"); // section header
